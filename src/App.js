d1d2c241dac12ab4aafbe21461642184ea272023
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-10 21:50:22 +0500

    Initialize app comonent with store

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Books from './components/books/Books';
 import Categories from './components/categories/categories';
 import Navbar from './components/navbar/Navbar';
+import store from './redux/configureStore';
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Switch>
-      <Route exact path="/categories">
-        <Categories />
-      </Route>
-      <Route exact path="/">
-        <Books />
-      </Route>
-    </Switch>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/categories">
+          <Categories />
+        </Route>
+        <Route exact path="/">
+          <Books />
+        </Route>
+      </Switch>
+    </Router>
+  </Provider>
 );
 
 export default App;

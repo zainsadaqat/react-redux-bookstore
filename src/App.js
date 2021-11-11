9b3efec3ef4aa982be3a08d2ccafa11e47c1d84d
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-11-11 05:19:10 +0500

    Add endpoint

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from './components/categories/categories';
 import Navbar from './components/navbar/Navbar';
 import store from './redux/configureStore';
 
+export const ENDPOINT = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/OWE8y7l6VJxjIC8WGkOP/books';
+
 const App = () => (
   <Provider store={store}>
     <Router>
